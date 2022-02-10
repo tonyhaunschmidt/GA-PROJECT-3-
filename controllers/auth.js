4ca7dda728c8095711168c08b0a62892a2cb748b
Tony Haunschmidt <tmhaunschmidt@gmail.com>
2022-02-10 00:31:23 +0000

    added controllers, auth, secureroutes, reviews, user schema

---
+++ controllers/auth.js
@@ -0,0 +1,31 @@
+import User from '../models/user.js' 
+import jwt from 'jsonwebtoken' 
+import { secret } from '../config/environment.js' 
+
+export const registerUser = async (req, res) => {
+  try {
+    console.log(req.body)
+    const newUser = await User.create(req.body)
+    console.log(newUser)
+    return res.status(202).json({ message: 'Registration Successful' })
+  } catch (err) {
+    console.log(err)
+    return res.status(422).json(err)
+  }
+}
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const userToLogin = await User.findOne({ email: email })
+    if (!userToLogin || !userToLogin.validatePassword(password)){
+      return res.status(401).json({ message: 'Unauthorised' })
+    }
+    const token = jwt.sign({ sub: userToLogin._id }, secret, { expiresIn: '7 days' })
+    return res.status(200).json({ message: `Welcome back, ${userToLogin.username}`, token: token })
+  } catch (err) {
+    console.log(err)
+    return res.status(401).json(err)
+  }
+}
+
--- models/recipe.js
+++ models/recipe.js
@@ -1,9 +1,16 @@
 import mongoose from 'mongoose'
-import uniqueValidator from 'mongoose-unique-validator'
 
 
 
 const { Schema } = mongoose
+
+const reviewSchema = new Schema({
+  text: { type: String, required: true, maxlength: 300 },
+  rating: { type: Number, required: true, min: 1, max: 5 },
+  owner: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+}, {
+  timestamps: true,
+})
 
 const recipeSchema = new Schema({
   title: { type: String, required: true },
@@ -11,17 +18,30 @@ const recipeSchema = new Schema({
   cookingTime: { type: Number, required: true },
   mealType: { type: String, required: true },
   ingredients: [{ type: Object, required: true }],  ///SUBSCHEMA?
+  // [{ ingredient: eggs, quantityForOne: 2, quantityForTwo: 3, quantityForThree: 3,  quantityForFour: 4, measure: units }, { ingredients milk .... }]
   method: [{ type: Object, required: true }],
   //nutrition: {}
   image: { type: String },
   tags: [{ type: String }], 
-  //Owner
-  //Reviews
+  owner: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  reviews: [reviewSchema],
   Featured: { type: Boolean },
 }, {
   timestamps: true,
 })
 
 
+recipeSchema.virtual('avgRating')
+  .get(function(){
+    if (!this.reviews.length) return 'Not rated yet'
+    const sum = this.reviews.reduce((acc, review) => {
+      return acc + review.rating
+    }, 0)
+    return  (sum / this.reviews.length).toFixed(2)
+  })
+
+recipeSchema.set('toJSON', {
+  virtuals: true,
+})
 
 export default mongoose.model('Recipe', recipeSchema)
