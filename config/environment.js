7e3d9e159839cc39a43ca1820731654f8ade88dc
Tony Haunschmidt <tmhaunschmidt@gmail.com>
2022-03-21 14:08:13 +0000

    setting heroku deployment

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
 export const port = process.env.PORT || 4000
-export const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/GAProject3'
+export const dbURI = process.env.DB_URI || 'mongodb://127.0.0.1:27017/GAProject3'
 export const secret = process.env.SECRET || 'we are gonna smash project 3'
 
--- index.js
+++ index.js
@@ -3,6 +3,14 @@ import mongoose from 'mongoose'
 import router from './config/routes.js'
 import { port, dbURI } from './config/environment.js'
 
+import 'dotenv/config' // only needs to be added if it doesn't already exist
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
 const app = express()
 
 const startServer = async () => {
@@ -13,11 +21,17 @@ const startServer = async () => {
     app.use(express.json())
 
     app.use((req, _res, next) => {
-      console.log(`Request received: ${req.method} - ${req.url}`) 
+      console.log(`Request received: ${req.method} - ${req.url}`)
       next()
     })
 
     app.use('/api', router)
+
+    app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
 
     app.use((_req, res) => {
       return res.status(404).json({ message: 'Route Not Found' })
