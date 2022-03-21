7f9eaed927e1194852d807ec3dac19deac5f1d77
Tony Haunschmidt <tmhaunschmidt@gmail.com>
2022-03-21 12:51:24 +0000

    ready for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,4 @@
-export const port = 4000
-export const dbURI = 'mongodb://127.0.0.1:27017/GAProject3'
-export const secret = 'we are gonna smash project 3'
+export const port = process.env.PORT || 4000
+export const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/GAProject3'
+export const secret = process.env.SECRET || 'we are gonna smash project 3'
 
-// 27017
